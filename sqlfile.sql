e1b31c6fc7fcf066b660ddfe419149727a1d1efb
Hettore Eduardo <duh.barretoo@gmail.com>
2023-06-01 10:20:35 -0300

    Adição de arquivo para o desafio e resolução

--- sqlfile.sql
+++ sqlfile.sql
@@ -3,16 +3,16 @@ CREATE DATABASE ubs; /*  Comando para criar o banco de dados  */
 USE ubs; /*  Comando para usar/selecionar o banco de dados  */
 
 CREATE TABLE ubs_uf (
-codigo_uf int PRIMARY KEY not null,
-unidade_da_federacao varchar(50) NOT NULL,
-uf char(2) NOT NULL
+codigo_uf INT PRIMARY KEY NOT NULL,
+unidade_da_federacao VARCHAR(50) NOT NULL,
+uf CHAR(2) NOT NULL
 ); /*  Comando para criar uma tabela já incluindo o título da coluna obs: tabela ainda sem conteúdo  */
 
-insert INTO ubs_uf (
+INSERT INTO ubs_uf (
 codigo_uf,
 unidade_da_federacao,
-uf) values (52, 'Goiás', 'GO'); 
-/*  Comando para inserir dados manualmente, necessário informar o título da coluna e depois passar os valores   */
+uf) VALUES (52, 'Goiás', 'GO'); 
+/* Comando para inserir dados manualmente, necessário informar o título da coluna e depois passar os valores */
 
 UPDATE ubs_uf SET uf='SP' WHERE codigo_uf=35; 
 /*  Comando para atualizar um registro especifico usando o WHERE  */
@@ -27,17 +27,17 @@ SELECT COUNT(*) as TOTAL_DE_LINHAS FROM data_ubs; /*  Comando para contar a  qua
 
 truncate table ubs_uf; /*  Comando para apagar todo o conteúdo de uma tabela  */
 
-truncate table data_ubs;
+TRUNCATE TABLE data_ubs;
 /* Apaga todas as linhas de uma tabela */
 
-drop table data_ubs; 
+DROP TABLE data_ubs; 
 /*  Comando para apagar uma tabela inteira  */
 
 ALTER DATABASE ubs DEFAULT CHARACTER SET utf8mb4 COLLATE utf8_general_ci; /*  Comando para mudar o encoding  */
  
 SELECT * FROM ubs_uf where unidade_da_federacao = 'Rondônia'; 
 
-DELETE from ubs_uf where codigo_uf='52'; 
+DELETE FROM ubs_uf WHERE codigo_uf='52'; 
 /*  Comando para apagar uma linha que tenha um conteúdo especifico usando o where  */
 
 CREATE TABLE data_ubs (
@@ -50,6 +50,22 @@ BAIRRO VARCHAR(60) NOT NULL,
 LATITUDE VARCHAR(100) NOT NULL,
 LONGITUDE VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE data_municipios (
+UF_MUNICIPIO VARCHAR(100) NOT NULL,
+IBGE INT PRIMARY KEY NOT NULL,
+IBGE7 INT NOT NULL,
+UF CHAR(2) NOT NULL,
+MUNICIPIO VARCHAR(100) NOT NULL,
+REGIAO VARCHAR(50) NOT NULL,
+POPULACAO_2010 LONG NOT NULL,
+PORTE VARCHAR(50) NOT NULL,
+CAPITAL VARCHAR(30)
+);
+
+SELECT * FROM data_municipios; 
+
+TRUNCATE data_municipios;
 
 /* 
 Atividade - 1
@@ -67,20 +83,20 @@ nordeste = 15463 unidades.
 */
 SELECT * FROM ubs_uf; /*  Mostrar os estados e seus respectivos códigos sendo sp=35 e estados do nordeste de 21 à 29*/
 
-SELECT COUNT(*) as TOTAL_UBS_SP FROM data_ubs WHERE UF=35; 
+SELECT COUNT(*) AS TOTAL_UBS_SP FROM data_ubs WHERE UF=35; 
 /*  Faz a contagem de quantas linhas cadastradas que tenham uf=35 (São Paulo)  */
 
-SELECT COUNT(*) as TOTAL_UBS_NORDESTE FROM data_ubs WHERE uf BETWEEN 21 AND 29; 
+SELECT COUNT(*) AS TOTAL_UBS_NORDESTE FROM data_ubs WHERE uf BETWEEN 21 AND 29; 
 /*  Faz a contagem de todas as linhas cadastradas que tenham uf=21;22;23;24;25;26;27;28 e 29  */
 
 /*  testes  */
-SELECT uf.uf as Estado_ou_Região, count(dt.CNES) as Total_UBS
+SELECT uf.uf AS Estado_ou_Região, count(dt.CNES) AS Total_UBS
 FROM data_ubs AS dt
 INNER JOIN ubs_uf AS uf
 ON uf.codigo_uf = dt.uf
 WHERE uf.uf = 'sp'
 UNION
-SELECT 'Nordeste', SUM(Total_UBS) FROM (SELECT uf.uf, count(dt.CNES) as Total_UBS
+SELECT 'Nordeste', SUM(Total_UBS) FROM (SELECT uf.uf, count(dt.CNES) AS Total_UBS
 FROM data_ubs AS dt
 INNER JOIN ubs_uf AS uf
 ON uf.codigo_uf = dt.uf
@@ -96,10 +112,10 @@ uf=11 - rondonia - centro=41, total=281
 uf=12 - Acre - centro=27, total= 228
 */
 
-SELECT uf as Estado, COUNT(*) as total_ubs_centro FROM data_ubs WHERE UF = 11 AND bairro = 'centro';
-/* Consulta a sigla do estado e retorna com a condição de ser 11 que é São Paulo e ser bairro centro */
+SELECT uf AS Estado, COUNT(*) AS total_ubs_centro FROM data_ubs WHERE UF = 11 AND bairro = 'centro';
+/* Consulta a sigla do estado e retorna com a condição de ser 11 que é Rondônia e ser bairro centro */
 
-SELECT uf as CODIGO_UF, COUNT(*) as total_ubs_estado FROM data_ubs WHERE UF = 11;
+SELECT uf AS CODIGO_UF, COUNT(*) AS total_ubs_estado FROM data_ubs WHERE UF = 11;
 /* Consulta a sigla do estado e retorna o total de ubs do estado */
 
 SELECT COUNT(*) AS qtd_ubs_centro FROM data_ubs WHERE UF = 12 AND bairro = 'centro';
@@ -111,7 +127,7 @@ SELECT UF, count(UF) AS quantidade_ubs FROM data_ubs group by UF
 UNION
 SELECT UF, count(BAIRRO) AS quantidade_ubs_centro FROM data_ubs WHERE BAIRRO='CENTRO' group by UF order by UF;
 
-select uf, count(uf);
+SELECT uf, count(uf);
 
 /*  Da certo com o total!  */
 SELECT data_ubs.uf AS UF, ubs_uf.unidade_da_federacao AS ESTADO,
@@ -127,11 +143,11 @@ INNER JOIN ubs_uf ON data_ubs.uf = ubs_uf.codigo_uf
 WHERE data_ubs.bairro <> "centro"
 GROUP BY data_ubs.uf, ubs_uf.codigo_uf;
 
-ALTER TABLE ubs_uf ADD primary key (codigo_uf);
+ALTER TABLE ubs_uf ADD PRIMARY KEY (codigo_uf);
 /*  Comando para adicionar um campo como chave primária  */
 
 ALTER TABLE ubs_uf 
-CHANGE uf uf int;
+CHANGE uf CIDADE int;
 /* Comando para alterar o nome e o tipo de dado */
 
 ALTER TABLE ubs_uf
@@ -147,6 +163,25 @@ DESAFIO: Observe nos dados das UBSs que existe uma coluna intitulada “IBGE”.
 um relatório que liste todas as UBS de um respectivo município/distrito/subdistrito.
 R:
 DICA: Observe como são estruturados os dados que descrevem os municípios/distrito/subdistritos elencados pelo IBGE. */
+
+SELECT * FROM data_ubs; /*  Comando para mostrar todo o conteúdo de uma tabela  */
+
+SELECT uf.uf as Estado, dt.IBGE as Cod_cidade, dt.NOME as Nome_da_UBS
+FROM data_ubs AS dt
+INNER JOIN ubs_uf AS uf
+ON uf.codigo_uf = dt.uf
+WHERE dt.IBGE = 355620;
+
+ /* cod valinhos = 355620 
+ Baixei uma lista q envolve o numero do ibge e nome do municipio */ 
+
+SELECT du.UF AS Cod_Estado, du.IBGE AS Cod_IBGE_cidade, dt.MUNICIPIO AS CIDADE, du.CNES AS CNES, du.NOME
+FROM data_municipios AS dt
+INNER JOIN data_ubs AS du
+ON dt.IBGE = du.IBGE
+WHERE dt.MUNICIPIO = 'Rio de janeiro';
+
+SELECT * FROM data_municipios;
 
 /* 
 DDL - Data Definition Language - Linguagem de Definição de Dados.
